c4aa180172d67bc276713d7abafe7c0664ffa0e4
“nameisjar” <nthia4@gmail.com>
2025-05-09 21:02:58 +0700

    add projek desktop

---
+++ second_win.py
@@ -0,0 +1,11 @@
+# write a code for the second app
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
+    
+class TestWin(QWidget):
+    def __init__(self):
+        pass
